2500cfb546bf897f74f463695159a0421b746a78
Justus Grunow <jg@justus.ws>
2025-01-23 21:47:49 -0500

    Writing generator in python. MVP working

--- build.pl
+++ build.pl
@@ -1,12 +1,32 @@
 #!/usr/bin/perl
-
+use strict;
+use warnings;
 use feature qw(say);
-use Text::Markup::Markdown;
+use Data::Dumper;
+#use Text::Markup::Markdown;
+use YAML::PP;
+my $ypp = YAML::PP->new;
 
-my $html = Text::Markup->new->parse(file => 'menu.md');
+#my $html = Text::Markup->new->parse(file => 'menu.md');
+my %menu = $ypp->load_file("menu.yaml");
 
 
 open my $fh, ">", "index.html";
+my $html = <<~EoF;
+	<html>
+	<head>
+	<title>Cocktail Menu</title>
+	<link rel="stylsheet" href="style.css">
+	</head>
+	<body>
+	EoF
+
+$html .= "Hi";
+
+#say Dumper(@menu);
+say "Menu";
+say Dumper($menu{menu});
+
 print $fh $html;
 
 
