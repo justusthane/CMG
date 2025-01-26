4c312cc3f7481d23c447afbc8ff2b5e42aa30193
Justus Grunow <jg@justus.ws>
2025-01-25 22:16:21 -0500

    Directions now correctly converted from Markdown to HTML

--- build.py
+++ build.py
@@ -13,6 +13,15 @@ with open('menu.yaml', 'r') as file:
 templateMenu = env.get_template("menu.html")
 templateRecipes = env.get_template("recipes.html")
 
+for bev in menu['menu']:
+    #print(bev)
+    for name in bev:
+        print(name)
+        for ingredient in bev[name]['ingredients']:
+            print(f'<li>{ingredient[list(ingredient)[0]]} {list(ingredient)[0]}</li>')
+        print(markdown.markdown(bev[name]['directions']))
+        bev[name]['directions_html'] = markdown.markdown(bev[name]['directions'])
+
 with open('index.html', 'w') as file:
     file.write(templateMenu.render(bevs = menu['menu']))
 
@@ -21,13 +30,6 @@ with open('recipes.html', 'w') as file:
 
 print(menu['menu'])
 
-for bev in menu['menu']:
-    #print(bev)
-    for name in bev:
-        print(name)
-        for ingredient in bev[name]['ingredients']:
-            print(f'<li>{ingredient[list(ingredient)[0]]} {list(ingredient)[0]}</li>')
-        print(markdown.markdown(bev[name]['directions']))
 
 for ingredient in menu['out_of_stock']:
     print(ingredient)
